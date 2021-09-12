2e187a0c4c84985831b122dea121c86d4dd4c378
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2021-09-12 14:26:42 +0200

    Fix update_manifest script and update release drafter (#50)
    * Update release drafter
    
    * Fix update_manifest script
    
    * patch
    
    * Merge hacs validate and hassfest into one file
    
    * Patch

--- .github/scripts/update_manifest.py
+++ .github/scripts/update_manifest.py
@@ -14,15 +14,26 @@ def update_manifest():
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
+    print("Version number being inserted: " + str(version))
+
+    print("Opening file...")
+
     with open(f"{os.getcwd()}/custom_components/toyota/manifest.json") as manifestfile:
         manifest = json.load(manifestfile)
 
     manifest["version"] = version
 
+    print("Manifest file after inserting new version number:")
+    print(manifest)
+
+    print("Saving file...")
+
     with open(
         f"{os.getcwd()}/custom_components/toyota/manifest.json", "w"
     ) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
+
+    print("Done!")
 
 
 update_manifest()
