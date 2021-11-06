d9080bb559483ed1fddebd710e169f762f384f89
DurgNomis-drol <simongrud@gmail.com>
2021-11-06 10:58:21 +0100

    Remove v from version string

--- .github/scripts/update_manifest.py
+++ .github/scripts/update_manifest.py
@@ -9,19 +9,21 @@ import sys
 
 def update_manifest():
     """Update the manifest file."""
-    version = "0.0.0"
+    version = "v0.0.0"
     for index, value in enumerate(sys.argv):
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
-    print("Version number being inserted: " + str(version))
+    version_striped = version.replace("v", "")
+
+    print("Version number being inserted: " + str(version_striped))
 
     print("Opening file...")
 
     with open(f"{os.getcwd()}/custom_components/toyota/manifest.json") as manifestfile:
         manifest = json.load(manifestfile)
 
-    manifest["version"] = version
+    manifest["version"] = version_striped
 
     print("Manifest file after inserting new version number:")
     print(manifest)
