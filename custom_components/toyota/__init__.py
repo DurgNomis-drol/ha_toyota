32eac06b0fddfe1ba64e75aedff3ddf72a57c5a5
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2023-02-22 19:47:16 +0100

    Use async_forward_entry_setups

--- custom_components/toyota/__init__.py
+++ custom_components/toyota/__init__.py
@@ -162,7 +162,7 @@ async def async_setup_entry(  # pylint: disable=too-many-statements
         raise ConfigEntryNotReady
 
     # Setup components
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
