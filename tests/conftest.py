f7489da03d48e6bd157d54d277e2e04cf033238c
Simon Hörrle <simon.hoerrle@dm.de>
2023-11-24 11:33:20 +0100

    add initial test files

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from homeassistant import loader
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.area_registry import AreaRegistry
+from homeassistant.helpers.device_registry import DeviceRegistry
+from homeassistant.helpers.entity_registry import EntityRegistry
+from pytest_homeassistant_custom_component.common import (
+    mock_area_registry,
+    mock_device_registry,
+    mock_registry,
+)
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations: bool) -> None:
+    yield
+
+
+@pytest.fixture
+def enable_custom_integrations(hass: HomeAssistant) -> None:
+    """Enable custom integrations defined in the test dir."""
+    hass.data.pop(loader.DATA_CUSTOM_COMPONENTS)
+
+
+@pytest.fixture
+def area_reg(hass: HomeAssistant) -> AreaRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_area_registry(hass)
+
+
+@pytest.fixture
+def device_reg(hass: HomeAssistant) -> DeviceRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_device_registry(hass)
+
+
+@pytest.fixture
+def entity_reg(hass: HomeAssistant) -> EntityRegistry:
+    """Return an empty, loaded, registry."""
+    return mock_registry(hass)
